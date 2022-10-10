55a8160e08bacc4251cf876d044f19ddcdc1cd52
azm89 <ashtonmoore89@gmail.com>
2022-10-10 11:28:54 -0700

    rewrote server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Require express and mongoose
 const express = require('express');
 const mongoose = require('mongoose');
 
@@ -6,15 +7,19 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-media-api', {
+// Connect mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
   useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-// Use this to log mongo queries being executed!
+// Log mongoose queries
 mongoose.set('debug', true);
 
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+app.listen(PORT, () => console.log(` ************** Connected on localhost:${PORT} ************* `));
